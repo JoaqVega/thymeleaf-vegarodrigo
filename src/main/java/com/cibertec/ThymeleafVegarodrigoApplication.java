5ad20c24128cd9e322f7bc94f1ec200916c92609
Rodrigo <124454510+JoaqVega@users.noreply.github.com>
2025-07-04 20:51:42 -0500

    Add files via upload

---
+++ src/main/java/com/cibertec/ThymeleafVegarodrigoApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafVegarodrigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafVegarodrigoApplication.class, args);
+	}
+
+}
